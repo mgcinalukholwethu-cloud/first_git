7867bb698b3a26f0c8caa345a9df3f8eb05ac028
mgcinalukholwethu-cloud <mgcinalukholwethu@gmail.com>
2025-10-31 15:04:35 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,21 @@
+
+// TODO: Create an empty array called colors
+let colors = [];
+
+// TODO: Add three colors to the array using push()
+colors.push("red");
+colors.push("blue");
+colors.push("green");
+
+// TODO: Create another array called numbers with five numbers
+let numbers = [10, 20, 30, 40, 50];
+
+// TODO: Remove the last color from the colors array
+colors.pop(green);
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("yellow");
+
+// TODO: Print the length of both arrays
+console.log("Length of colors array:", colors.length);
+console.log("Length of numbers array:", numbers.length);
