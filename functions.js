1de149de758131951a6b674a25037b15f489c325
mgcinalukholwethu-cloud <mgcinalukholwethu@gmail.com>
2025-11-06 09:15:24 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+//Create a function called greet that takes a name peremeter//
+function greet(name){
+    return 'Hello,${name}!';
+}
+// Create a function called add that takes two parameters
+function add(a,b){
+    return a + b;
+}
+//Call both functions and store their results in variables
+const greeting =greet("Lukho");
+const sum= add(7,5);
+
+//Print the results to the console
+console.log(greetring); //Output: Hello,Lukho!
+console.log('Sum:',sum); // Output: sum:12
